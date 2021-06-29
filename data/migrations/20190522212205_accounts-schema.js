518597182312eb955fa0fc683ff19d147073dfdd
Rigo Limon <limon1285@gmail.com>
2021-06-29 16:51:11 -0500

    added users database and foreign keys to join tables to accounts

--- data/migrations/20190522212205_accounts-schema.js
+++ data/migrations/20190522212205_accounts-schema.js
@@ -1,15 +1,29 @@
-
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable('accounts', tbl => {
-    tbl.increments();
-    tbl.string('name')
-      .notNullable()
-      .unique();
-    tbl.decimal('budget')
-      .notNullable();
-  });
+exports.up = function (knex, Promise) {
+  return (
+    knex.schema
+      .createTable("users", (tbl) => {
+        tbl.increments("id");
+        tbl.string("name", 128).notNull().unique();
+        tbl.integer("age", 128).notNull();
+        tbl.string("location", 128).notNull();
+      })
+      //will have a foreign key on KNEXFILE.JS
+      .createTable("accounts", (tbl) => {
+        tbl.increments();
+        tbl.string("name").notNullable().unique();
+        tbl.decimal("budget").notNullable();
+        tbl
+          .integer("user_id")
+          .unsigned()
+          .notNull()
+          // OR IT can be .references('id').inTable('users), no need for table
+          .references("users.id")
+          .onDelete("CASCADE")
+          .onUpdate("CASCADE");
+      })
+  );
 };
 
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('accounts');
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTableIfExists("accounts").dropTableIfExists("users");
 };
